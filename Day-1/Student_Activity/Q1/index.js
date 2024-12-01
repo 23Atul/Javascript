cd49fe2b44d375a3c449fddecb0f1b1876cbaa78
23Atul <atulraman23@gmail.com>
2024-12-02 00:31:09 +0530

    Q1

---
+++ Day-1/Student_Activity/Q1/index.js
@@ -0,0 +1,12 @@
+// Coding Challenge 1: Write a JavaScript function to calculate the area of a rectangle.The function should take two parameters, width and height, and return the calculated area.
+
+function areaOfRectangle(width, height)
+{
+    return width*height;
+}
+
+var width=10;
+var height=35;
+
+area = areaOfRectangle(width,height)
+console.log(area);
