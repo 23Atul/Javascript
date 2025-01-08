e0889287f292452290702ee7c24e050a0a58809a
23Atul <atulraman23@gmail.com>
2025-01-08 16:41:40 +0530

    callback function

---
+++ Day1/lecture-1/callbacks.js
@@ -0,0 +1,55 @@
+
+// --------- C A L L   B A C K   F U N C T I O N ---------
+
+// FUNCTION STATEMENT
+function a(){
+    console.log("Hi");
+}
+
+a();
+
+
+// FUNCTION EXPRESSION // Ananomous Function
+let b = function(){
+    console.log("Hello");
+}
+
+b();
+
+
+// ARROW FUNCTION
+let c = () =>{
+    console.log("Namaste");
+}
+
+c();
+
+
+// IIFE -- Immdeiatelt Invoked Function Expression
+(()=>{
+        console.log("Atul");
+})();
+
+// OR
+
+(function(){
+    console.log("Raman");
+})();
+
+
+// call back function -- when a function is called from inside of another function then the called function is called call back function; and the calling function is called HOF (higher order function)
+
+//HOF // when a function accepts function as an parameter then it is called HOF
+function washClothes(name, count, cb) {  // cb==ironClothes
+    console.log(`${name}'s ${count} clothes are washed`);
+    cb(name,count); // we can pass value as well inside call back
+}
+
+// CB function 
+function ironClothes(name,count){
+    console.log(`${name}'s ${count} clothes are ironed as well`);
+
+}
+
+
+washClothes("Atul", 20, ironClothes) // passing function as argument, do not invoke here
