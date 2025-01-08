c864856fdbb1743b862b85256ed293a08e3a4904
23Atul <atulraman23@gmail.com>
2025-01-09 00:04:57 +0530

    Q6c

--- Day2/Assignments/Q6/index.js
+++ Day2/Assignments/Q6/index.js
@@ -7,7 +7,7 @@ let str1 = "1 2 3 4";
 let arr1 = str1. split(" "); // string to array, but elements are still string
 
 let res1 = arr1.map((el) => el*2);  // string converts to number explicitly
-// console.log(res1);
+console.log(res1);
 
 
 
@@ -17,7 +17,7 @@ let res1 = arr1.map((el) => el*2);  // string converts to number explicitly
 // • Expected Output: "[2, 4]"
 
 let str2 = "1, 2, 3, 4";
-let arr2 =  str2.split(",").map(Number)
+let arr2 = str2.split(",").map(Number)  // Convert all elements to numbers
 
 let res2 = arr2.filter((el)=> el%2==0);
 console.log((res2));
@@ -27,3 +27,9 @@ console.log((res2));
 // • "Prompt the user for a series of numbers separated by semicolons. Implement a function that uses the reduce HOF to calculate the sum of the numbers and display the result."
 // • Example Input: "5; 10; 15"
 // • Expected Output: "30"
+
+let str3 = "5; 10; 15";
+let arr3 = str3.split(";").map(Number);
+let res3= arr3.reduce((acc,el)=> acc+el);
+console.log(res3);
+
