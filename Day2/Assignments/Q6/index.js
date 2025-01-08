e035a7603e85ad4e23594f4cedc59bca7e06d02e
23Atul <atulraman23@gmail.com>
2025-01-08 23:48:07 +0530

    Q6a

---
+++ Day2/Assignments/Q6/index.js
@@ -0,0 +1,24 @@
+// 1. Mapping Array Elements:
+// • "Prompt the user to input a list of numbers separated by spaces. Implement a function that uses the map HOF to double each number and display the transformed array."
+// • Example Input: "1 2 3 4"
+// • Expected Output: "[2, 4, 6, 8]"
+
+let str = "1 2 3 4";
+let arr1 = str. split(" "); // string to array, but elements are still string
+
+let res1 = arr1.map((el) => el*2);  // string converts to number explicitly
+console.log(res1);
+
+
+
+// 2. Filtering Data:
+// • "Ask the user for a list of integers separated by commas. Create a function that uses the filter HOF to extract even numbers from the input and display them."
+// • Example Input: "1, 2, 3, 4, 5"
+// • Expected Output: "[2, 4]"
+
+
+
+// 3. Reducing Arrays:
+// • "Prompt the user for a series of numbers separated by semicolons. Implement a function that uses the reduce HOF to calculate the sum of the numbers and display the result."
+// • Example Input: "5; 10; 15"
+// • Expected Output: "30"
